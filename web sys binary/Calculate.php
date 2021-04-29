4b72339884d32921de608698a5a6955149916fd5
Maeangelanavarro <79089242+Maeangelanavarro@users.noreply.github.com>
2021-04-29 22:13:21 +0800

    Add files via upload

---
+++ web sys binary/Calculate.php
@@ -0,0 +1,26 @@
+<?php
+class Calculate{
+
+    function binary($binaryone)
+
+    {   
+        
+        echo $sum;
+        if ($binaryone > 1)
+       $result = decbin($binaryone/2);
+    
+        echo $result;
+        //$sum=0;
+        //while($binaryone > 0) {
+
+            //sum + $binaryone ^ 1;
+            //x=1;
+        }
+
+         
+
+
+        
+
+    }
+}
---
+++ web sys binary/process.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'Calculate.php';
+
+$obj = new calculate;
+
+$obj->binary($_POST ['binaryone']) ;
